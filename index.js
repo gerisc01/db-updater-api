f329143c2415fe1bb37bcfdb2671cd4977592635
Scott Gerike <gerisc01@luther.edu>
2017-10-12 16:20:56 -0500

    Look for port in env variable; default to 3000 if not found.

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.put('/api/v1/gistdb/:gistId/:fileName', function (req, res) {
   })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port '+(process.env.PORT || 3000))
 })
